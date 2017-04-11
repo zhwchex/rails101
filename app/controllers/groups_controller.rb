f273cef07e08c768357ecdd2fe2b7433fc662bba
Phoenix <zhwchex@gmail.com>
2017-04-11 16:05:39 +0800

    check owner permission when access edit/update/destroy

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new, :create]
+    before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
   def index
     @groups = Group.all
   end
@@ -8,14 +8,18 @@ class GroupsController < ApplicationController
      @group = Group.find(params[:id])
    end
 
-  def edit
-   @group = Group.find(params[:id])
-  end
+   def edit
+     @group = Group.find(params[:id])
+
+     if current_user != @group.user
+       redirect_to root_path, alert: "You have no permission."
+     end
+   end
 
   def new
     @group = Group.new
   end
-  
+
     def create
       @group = Group.new(group_params)
       @group.user = current_user
@@ -27,22 +31,30 @@ class GroupsController < ApplicationController
       end
     end
 
-  def update
-  @group = Group.find(params[:id])
-  if @group.update(group_params)
-    redirect_to groups_path, notice: "Update Success"
-  else
-    render :edit
-  end
-end
+    def update
+      @group = Group.find(params[:id])
 
+      if current_user != @group.user
+        redirect_to root_path, alert: "You have no permission."
+      end
 
-  def destroy
-    @group = Group.find(params[:id])
-    @group.destroy
-    flash[:alert] = "Group deleted"
-    redirect_to groups_path
-  end
+      if @group.update(group_params)
+        redirect_to groups_path, notice: "Update Success"
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @group = Group.find(params[:id])
+
+      if current_user != @group.user
+        redirect_to root_path, alert: "You have no permission."
+      end
+
+      @group.destroy
+      redirect_to groups_path, alert: "Group deleted"
+    end
 
   private
 
